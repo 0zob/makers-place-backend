2752ea82e3be9352e5cec098c3b23a46251b80b1
enzotorrs <enzotorres969@gmail.com>
2022-11-15 14:02:30 -0300

    feat: add route to index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const config = require('./config')
 const express = require('express')
 const conn = require('./db/conn')
+const UserRoutes = require('./routes/UserRoutes')
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/users', UserRoutes)
 
 conn.sync()
     .then(() => {
