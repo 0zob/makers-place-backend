497ac2360981f798677f6476a4cc00f329acfb15
enzotorrs <enzotorres969@gmail.com>
2022-11-15 15:38:32 -0300

    feat: add login endpoint

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/User/UserController')
 const router = Router()
 
 router.post('/register', UserController.createUser)
+router.post('/login', UserController.loginUser)
 
 module.exports = router
