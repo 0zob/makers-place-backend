2e2baf655be2c557bacc18d398dfb9bede6e5571
enzotorrs <enzotorres969@gmail.com>
2022-11-15 17:29:26 -0300

    feat: add helper to get token from header

---
+++ helpers/get-token-from-header.js
@@ -0,0 +1,11 @@
+function getTokenFromHeader(req) {
+    if (!req.headers.authorization) {
+        return null
+    }
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(" ")[1]
+
+    return token
+}
+
+module.exports = getTokenFromHeader
